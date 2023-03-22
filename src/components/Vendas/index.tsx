a15eda5b3ca606dce7f2030b1944f0763dcf628f
asilva <adriano@gescob.com.br>
2023-03-21 22:06:30 -0300

    Adicionado na função de desconto a atualização do valor total da compra ao adicionar o desconto

--- src/components/Vendas/index.tsx
+++ src/components/Vendas/index.tsx
@@ -184,10 +184,20 @@ export function Vendas() {
   }, []);
 
   useEffect(() => {
-    let valorTotalCompra = totalCompra;
+    let valorTotalCompra:number = totalCompra;
+
+    const labelTotalVenda = document.getElementById("venTotal");
+    const labelValorVenda = document.getElementById("valorTotalVenda");
 
     if (parseFloat(valorDesconto) > 0) {
+      
       valorTotalCompra = valorTotalCompra - parseFloat(valorDesconto);
+      if (labelTotalVenda) labelTotalVenda.innerHTML = mascaraValorMoedaBrasileira(valorTotalCompra);
+      if (labelValorVenda) labelValorVenda.innerHTML = mascaraValorMoedaBrasileira(valorTotalCompra);
+    }
+    else{
+      if (labelTotalVenda) labelTotalVenda.innerHTML = mascaraValorMoedaBrasileira(totalCompra);
+      if (labelValorVenda) labelValorVenda.innerHTML = mascaraValorMoedaBrasileira(totalCompra);
     }
 
     const valorTroco = parseFloat(valorPago) - valorTotalCompra;
