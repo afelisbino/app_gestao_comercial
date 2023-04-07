5245f09a2db5f7770dd94bb0e57bad95f83c516b
asilva <adriano@gescob.com.br>
2023-04-07 11:43:56 -0300

    Adicionado funçoes de validação de documentos e calculos de lucros

--- src/controllers/NumeroController.ts
+++ src/controllers/NumeroController.ts
@@ -1,11 +1,27 @@
 export function mascaraValorMoedaBrasileira(valor: number): string {
-    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
 
 export function removeMascaraMoedaBrasileira(valor: string): number {
-    return parseFloat(valor.replace(/[^0-9,-]/g, '').replace(',', '.'));
+  return parseFloat(valor.replace(/[^0-9,-]/g, "").replace(",", "."));
 }
 
-export function formatarValorMoeda(valor: string): string {
-    return valor.replace(",", ".");
+export function formataValorMoeda(valor: string): string {
+  return valor.replace(",", ".");
+}
+
+export function calculaPorcentagemLucroProduto(
+  precoVenda: number = 0,
+  valorCompra: number = 0
+): number {
+  return precoVenda > 0 && valorCompra > 0
+    ? ((precoVenda - valorCompra) / precoVenda) * 100
+    : 0;
+}
+
+export function calculaValorLucroProduto(
+  precoVenda: number = 0,
+  valorCompra: number = 0
+): number {
+  return precoVenda > 0 && valorCompra > 0 ? precoVenda - valorCompra : 0;
 }
