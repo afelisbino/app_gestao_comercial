6ba2e360f35452a74887094fc07db4298dc151d3
asilva <adriano@gescob.com.br>
2023-04-14 13:11:16 -0300

    Ajustes de componentes de carregamentos

--- src/components/Loaders/Spinner.tsx
+++ src/components/Loaders/Spinner.tsx
@@ -1,6 +1,6 @@
 export function Spinner() {
   return (
-    <div className="d-flex justify-content-center mt-5">
+    <div className="d-flex justify-content-center">
       <div className="spinner-border" role="status">
         <span className="visually-hidden">Carregando...</span>
       </div>
