a338a0a513a043cdf9b696145f7e2a9c007d4ed7
asilva <adriano@gescob.com.br>
2023-04-17 23:14:38 -0300

    Ajustes da importação dinamica das telas

--- src/contents/Home/index.tsx
+++ src/contents/Home/index.tsx
@@ -3,16 +3,18 @@ import Menu from "../../components/Menu";
 import { Spinner } from "../../components/Loaders/Spinner";
 
 const Home = () => {
-  const [menuSelecionado, selecionarMenu] = useState<string>("../Categorias");
+  const [menuSelecionado, selecionarMenu] = useState<string>("Venda/index.tsx");
 
-  const Tela = lazy(() => import(menuSelecionado));
+  const Tela = lazy(
+    async () => await import(`../${menuSelecionado}`)
+  );
 
   return (
     <>
       <header>
         <Menu selecionarOpcaoMenu={selecionarMenu} />
       </header>
-      <main className="container-fluid pt-5">
+      <main className="container-fluid pt-5 mt-5">
         <Suspense fallback={<Spinner />}>
           <Tela />
         </Suspense>
