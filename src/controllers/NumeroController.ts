da30604fd9054ab2e77822615354dfc50892f12c
asilva <adriano@gescob.com.br>
2023-04-17 23:15:14 -0300

    Remoção de metodo nao utilizado

--- src/controllers/NumeroController.ts
+++ src/controllers/NumeroController.ts
@@ -1,19 +1,27 @@
-export function mascaraValorMoedaBrasileira(valor: number): string {
+export function formataValorMoedaBrasileira(valor: number): string {
   return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
 
-export function removeMascaraMoedaBrasileira(valor: string): number {
+export function removeFormatoMoedaBrasileira(valor: string): number {
   return parseFloat(valor.replace(/[^0-9,-]/g, "").replace(",", "."));
 }
 
-export function formataValorMoeda(valor: string): string {
-  return valor.replace(",", ".");
+export function adicionaMascaraValor(valor: string): string {
+  const valorNumber: number = parseFloat(valor.replace(",", "."));
+
+  return !isNaN(valorNumber)
+    ? valorNumber.toLocaleString("pt-BR", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })
+    : "";
 }
 
 export function calculaPorcentagemLucroProduto(
   precoVenda: number = 0,
   valorCompra: number = 0
 ): number {
+  console.log(precoVenda);
   return precoVenda > 0 && valorCompra > 0
     ? ((precoVenda - valorCompra) / precoVenda) * 100
     : 0;
